4447c456c9aaebe063ebc4fd8ea6275b7f2c0ef2
Surayya <Surayyamel@users.noreply.github.com>
2021-06-12 18:24:22 +0100

    updated pool declaration for db

--- db.js
+++ db.js
@@ -1,30 +1,30 @@
 const Pool = require('pg').Pool;
 require('dotenv').config();
 
-let pool;
-if (process.env.NODE_ENV === 'development') {
-    pool = new Pool({
-        user: 'surayyafenton',
-        password: process.env.DB_PASSWORD,
-        host: 'localhost',
-        port: 5432,
-        database: 'workout',
-    });
-} else if (process.env.NODE_ENV === 'test') {
-    pool = new Pool({
-        user: 'surayyafenton',
-        password: process.env.DB_PASSWORD,
-        host: 'localhost',
-        port: 2345,
-        database: 'workout_test',
-    });
-} else {
-    pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false,
-        },
-    });
-}
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 
 module.exports = pool;
+
+// if (process.env.NODE_ENV === 'development') {
+//     pool = new Pool({
+//         user: 'surayyafenton',
+//         password: process.env.DB_PASSWORD,
+//         host: 'localhost',
+//         port: 5432,
+//         database: 'workout',
+//     });
+// } else if (process.env.NODE_ENV === 'test') {
+//     pool = new Pool({
+//         user: 'surayyafenton',
+//         password: process.env.DB_PASSWORD,
+//         host: 'localhost',
+//         port: 2345,
+//         database: 'workout_test',
+//     });
+// } else {
+//}
